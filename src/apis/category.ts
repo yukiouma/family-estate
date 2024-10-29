c439bb01e3614d180b7d9c6d50e8cda77572618e
Yuki Chen <2624466175@qq.com>
2024-10-29 12:46:14 +0800

    fix: update list category with sub

--- src/apis/category.ts
+++ src/apis/category.ts
@@ -11,20 +11,14 @@ export async function listCategories(): Promise<{ id: number; name: string }[]> 
 export async function listCategoryWithSub(): Promise<
   { id: number; name: string; subCategory: { id: number; name: string }[] }[]
 > {
-  return [
-    {
-      id: 1,
-      name: '流动资金',
-      subCategory: [
-        { id: 10, name: '活期' },
-        { id: 13, name: '余额宝' }
-      ]
-    },
-    { id: 2, name: '固定资产', subCategory: [{ id: 11, name: '汽车' }] },
-    { id: 3, name: '投资理财', subCategory: [{ id: 12, name: '基金' }] },
-    { id: 4, name: '应收款', subCategory: [] },
-    { id: 5, name: '负债', subCategory: [{ id: 14, name: '信用卡' }] }
-  ]
+  const result = []
+  const categories = await listCategories()
+  for (const category of categories) {
+    const { id, name } = category
+    const subCategory = await listSubCategories(id)
+    result.push({ id, name, subCategory })
+  }
+  return result
 }
 
 export async function listSubCategories(
